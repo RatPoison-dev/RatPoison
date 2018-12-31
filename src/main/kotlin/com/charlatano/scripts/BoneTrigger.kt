23b54774822589c0a97790a92e34e076988df291
AStupidRat <thedankestofmemerinos@gmail.com>
2018-12-31 04:51:05 -0600

    Removed formatting fuck up + cleanup

--- src/main/kotlin/com/charlatano/scripts/BoneTrigger.kt
+++ src/main/kotlin/com/charlatano/scripts/BoneTrigger.kt
@@ -16,12 +16,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.charlatano.scripts
+//package com.charlatano.scripts
 
 import com.charlatano.game.CSGO.clientDLL
 import com.charlatano.game.angle
 import com.charlatano.game.clientState
-import com.charlatano.game.entity.isScoped
 import com.charlatano.game.entity.position
 import com.charlatano.game.me
 import com.charlatano.game.offsets.ClientOffsets.dwForceAttack
@@ -31,9 +30,8 @@ import com.charlatano.utils.*
 import org.jire.arrowhead.keyReleased
 
 private val onBoneTriggerTarget = hook(1) {
-	if (ENABLE_BONE_TRIGGER) {
-		(findTarget(me.position(), clientState.angle(), false, BONE_TRIGGER_FOV, BONE_TRIGGER_BONE, false) >= 0)
-	}
+	if (ENABLE_BONE_TRIGGER)
+		findTarget(me.position(), clientState.angle(), false, BONE_TRIGGER_FOV, BONE_TRIGGER_BONE, false) >= 0
 	else false
 }
 
@@ -41,11 +39,8 @@ fun boneTrigger() = onBoneTriggerTarget {
 
 	if (keyReleased(FIRE_KEY)) {
 		if (LEAGUE_MODE) mouse(MOUSEEVENTF_LEFTDOWN) else clientDLL[dwForceAttack] = 5.toByte() //Mouse press
-
 		Thread.sleep(8 + randLong(16))
-
 		if (LEAGUE_MODE) mouse(MOUSEEVENTF_LEFTUP) else clientDLL[dwForceAttack] = 4.toByte() //Mouse release
-
 		Thread.sleep(8 + randLong(16))
 	}
 }
