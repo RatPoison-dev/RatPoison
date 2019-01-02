efda6c25a515a29830f28c1574de166f4546eae4
AStupidRat <thedankestofmemerinos@gmail.com>
2019-01-02 01:31:57 -0600

    Added bone trig enable key & bone trig shot delay

--- settings/BoneTrigger.kts
+++ settings/BoneTrigger.kts
@@ -1,21 +1,3 @@
-/*
- * Charlatano: Free and open-source (FOSS) cheat for CS:GO/CS:CO
- * Copyright (C) 2017 - Thomas G. P. Nappo, Jonathan Beaudoin
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 import com.charlatano.settings.*
 
 /**
@@ -32,3 +14,18 @@ BONE_TRIGGER_BONE = HEAD_BONE
  * Use aim when bone trigger activates, true by default
  */
 AIM_ON_BONE_TRIGGER = true
+
+/**
+ * Enable using a key for bone trigger
+ */
+BONE_TRIGGER_ENABLE_KEY = false
+
+/**
+ * Key to use bone trigger for if bone trigger key is enabled
+ */
+BONE_TRIGGER_KEY = 6
+
+/**
+ * Default minimum time in milliseconds to fire shots when requirements met
+ */
+BONE_TRIGGER_SHOT_DELAY = 8
