3fd748b68ed98d04a04caa1aedd85244597e1e73
AStupidRat <thedankestofmemerinos@gmail.com>
2019-01-02 06:37:28 -0600

    Added toggle esp script

---
+++ src/main/kotlin/com/charlatano/scripts/esp/ToggleESP.kt
@@ -0,0 +1,18 @@
+package com.charlatano.scripts.esp
+
+import com.charlatano.settings.ACTION_LOG
+import com.charlatano.settings.ENABLE_ESP
+import com.charlatano.settings.ESP_TOGGLE_KEY
+import com.charlatano.utils.every
+import org.jire.arrowhead.keyPressed
+
+fun espToggle() = every(4) {
+    if (keyPressed(ESP_TOGGLE_KEY)) {
+        ENABLE_ESP = !ENABLE_ESP
+        esp()
+        if (ACTION_LOG) {
+            println("ESP toggled to " + ENABLE_ESP)
+        }
+        Thread.sleep(500)
+    }
+}
