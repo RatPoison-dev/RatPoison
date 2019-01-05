6e49dcb3a1687f2eefc9ce00de22ce54da5a370d
AStupidRat <thedankestofmemerinos@gmail.com>
2019-01-05 16:12:57 -0600

    Added RCrosshair.kts file for RCROSSHAIR settings

---
+++ src/main/kotlin/rat/poison/settings/RCrosshair.kt
@@ -0,0 +1,11 @@
+package rat.poison.settings
+
+/**
+ * Width of bars, recommended 0.1 to 1.0
+ */
+var RCROSSHAIR_WIDTH = 1.0
+
+/**
+ * Length of bars, horizontally left to right, vertically top to bottom, recommended 5 to 10
+ */
+var RCROSSHAIR_LENGTH = 10
