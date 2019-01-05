1ff62c5e74015231d7687788a6a87f999dbda0f5
AStupidRat <thedankestofmemerinos@gmail.com>
2019-01-05 16:11:50 -0600

    added RCROSSHAIR_WIDTH + RCROSSHAIR_LENGTH settings

---
+++ settings/RCrosshair.kts
@@ -0,0 +1,11 @@
+import rat.poison.settings.*
+
+/**
+ * Width of bars, recommended 0.1 to 1.0
+ */
+RCROSSHAIR_WIDTH = 1.0
+
+/**
+ * Length of bars, horizontally left to right, vertically top to bottom, recommended 5 to 10
+ */
+RCROSSHAIR_LENGTH = 10
