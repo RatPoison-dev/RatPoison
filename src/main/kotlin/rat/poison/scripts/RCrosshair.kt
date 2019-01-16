1740e5564b1f51bc11a752b79be71223b2d927be
AStupidRat <thedankestofmemerinos@gmail.com>
2019-01-16 01:34:38 -0600

    Updated RCrosshair

--- src/main/kotlin/rat/poison/scripts/RCrosshair.kt
+++ src/main/kotlin/rat/poison/scripts/RCrosshair.kt
@@ -1,31 +1,29 @@
 package rat.poison.scripts
 
 import com.badlogic.gdx.graphics.Color
+import rat.poison.App
 import rat.poison.game.*
 import rat.poison.game.CSGO.gameHeight
 import rat.poison.game.CSGO.gameWidth
 import rat.poison.game.entity.*
-import rat.poison.overlay.RatPoisonOverlay
 import rat.poison.settings.*
 import rat.poison.utils.Vector
 
 private var lastpunch = Vector(0.0, 0.0, 0.0)
 
-internal fun rcrosshair() = RatPoisonOverlay {
-    if (!ENABLE_RECOIL_CROSSHAIR) return@RatPoisonOverlay
+internal fun rcrosshair() = App { //Currently not completely centered
+    if (!ENABLE_RECOIL_CROSSHAIR || MENUTOG) return@App
 
-    val x = ((gameWidth/2).toFloat() - ((gameWidth/90).toFloat()*lastpunch.y.toFloat()))
-    val y = ((gameHeight/2).toFloat() + ((gameHeight/90).toFloat()*lastpunch.x.toFloat()))
+    val x = ((gameWidth/2F+3F) - ((gameWidth/90F)*lastpunch.y.toFloat()))
+    val y = ((gameHeight/2F-1F) - ((gameHeight/90F)*lastpunch.x.toFloat()))
 
-    lastpunch = me.punch() //Punch seems to be one shot ahead so update it for the next shot
+    lastpunch = me.punch() //Punch is noticeable ahead (no traceray, isn't actually ahead), updating it one behind to make it easier to use and more reliable than before //remove this later
 
-    //Move to punch and vector eyes ?
-
-    shapeRenderer.apply sR@ {
+    shapeRenderer.apply {
         begin()
-        this@sR.color = Color.YELLOW
-        rect(x-RCROSSHAIR_LENGTH/2, y, RCROSSHAIR_LENGTH.toFloat(), RCROSSHAIR_WIDTH.toFloat())
-        rect(x, y-RCROSSHAIR_LENGTH/2, RCROSSHAIR_WIDTH.toFloat(), RCROSSHAIR_LENGTH.toFloat())
+        this.color = Color.YELLOW
+        rect(x-RCROSSHAIR_LENGTH/2F, y, RCROSSHAIR_LENGTH.toFloat(), RCROSSHAIR_WIDTH.toFloat())
+        rect(x, y-RCROSSHAIR_LENGTH/2F, RCROSSHAIR_WIDTH.toFloat(), RCROSSHAIR_LENGTH.toFloat())
         end()
     }
 }
