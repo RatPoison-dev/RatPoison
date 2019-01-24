ab30f6838601875e7d88a43bec54e77c36d8e220
AStupidRat <thedankestofmemerinos@gmail.com>
2019-01-24 04:17:33 -0600

    Bone trig set to only tap shots for now, better usage soon

--- src/main/kotlin/rat/poison/scripts/BoneTrigger.kt
+++ src/main/kotlin/rat/poison/scripts/BoneTrigger.kt
@@ -11,6 +11,7 @@ import rat.poison.settings.*
 import rat.poison.utils.*
 import org.jire.arrowhead.keyPressed
 import org.jire.arrowhead.keyReleased
+import rat.poison.game.entity.punch
 
 private val onBoneTriggerTarget = hook(1) {
 	if (ENABLE_BONE_TRIGGER)
@@ -20,10 +21,12 @@ private val onBoneTriggerTarget = hook(1) {
 
 fun boneTrigger() = onBoneTriggerTarget {
 
-	if ((keyReleased(FIRE_KEY) && BONE_TRIGGER_ENABLE_KEY && keyPressed(BONE_TRIGGER_KEY)) || (keyReleased(FIRE_KEY) && !BONE_TRIGGER_ENABLE_KEY)) {
-		if (LEAGUE_MODE) mouse(MOUSEEVENTF_LEFTDOWN) else clientDLL[dwForceAttack] = 5.toByte() //Mouse press
-		Thread.sleep(randLong(16))
-		if (LEAGUE_MODE) mouse(MOUSEEVENTF_LEFTUP) else clientDLL[dwForceAttack] = 4.toByte() //Mouse release
-		Thread.sleep(BONE_TRIGGER_SHOT_DELAY.toLong() /*+ randLong(16)*/)
+	if (me.punch().x == 0.0 && me.punch().y == 0.00) {
+		if ((keyReleased(FIRE_KEY) && BONE_TRIGGER_ENABLE_KEY && keyPressed(BONE_TRIGGER_KEY)) || (keyReleased(FIRE_KEY) && !BONE_TRIGGER_ENABLE_KEY)) {
+			if (LEAGUE_MODE) mouse(MOUSEEVENTF_LEFTDOWN) else clientDLL[dwForceAttack] = 5.toByte() //Mouse press
+			Thread.sleep(randLong(16))
+			if (LEAGUE_MODE) mouse(MOUSEEVENTF_LEFTUP) else clientDLL[dwForceAttack] = 4.toByte() //Mouse release
+			//Thread.sleep(BONE_TRIGGER_SHOT_DELAY.toLong() /*+ randLong(16)*/) //Make sure we can shoot an accurate shot
+		}
 	}
 }
--- src/main/kotlin/rat/poison/scripts/RCS.kt
+++ src/main/kotlin/rat/poison/scripts/RCS.kt
@@ -4,13 +4,13 @@ package rat.poison.scripts
 
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.math.Vector3
+import org.jire.arrowhead.keyPressed
 import rat.poison.game.*
 import rat.poison.game.entity.*
+import rat.poison.game.offsets.ClientOffsets
 import rat.poison.scripts.aim.bone
 import rat.poison.settings.*
-import rat.poison.utils.every
-import rat.poison.utils.normalize
-import rat.poison.utils.randDouble
+import rat.poison.utils.*
 
 private val lastPunch = Vector2()
 private val newPunch = Vector2()
@@ -24,12 +24,13 @@ fun rcs() = every(RCS_MIN_DURATION, RCS_MAX_DURATION) {
 	val shotsFired = me.shotsFired()
 	val forceSet = (shotsFired == 0 && !lastPunch.isZero)
 	if (forceSet || shotsFired > 0 /*|| weaponEntity.bullets() < 1*/) { //Fixes aim jumping down
+	//if (keyPressed(2)) {
 		val p = me.punch()
 		playerPunch.set(p.x.toFloat(), p.y.toFloat(), p.z.toFloat())
 		newPunch.set(playerPunch.x - lastPunch.x, playerPunch.y - lastPunch.y)
 		newPunch.scl((if (RCS_MAX > RCS_MIN) randDouble(RCS_MIN, RCS_MAX) else RCS_MIN).toFloat(),
 				(if (RCS_MAX > RCS_MIN) randDouble(RCS_MIN, RCS_MAX) else RCS_MIN).toFloat())
-		
+
 		val angle = clientState.angle()
 		angle.apply {
 			x -= newPunch.x
@@ -39,11 +40,18 @@ fun rcs() = every(RCS_MIN_DURATION, RCS_MAX_DURATION) {
 		clientState.setAngle(angle)
 		lastPunch.x = playerPunch.x
 		lastPunch.y = playerPunch.y
-		
+
+		if (keyPressed(2)) {
+			if (LEAGUE_MODE) mouse(MOUSEEVENTF_LEFTDOWN) else CSGO.clientDLL[ClientOffsets.dwForceAttack] = 5.toByte() //Mouse press
+			Thread.sleep(randLong(16))
+			if (LEAGUE_MODE) mouse(MOUSEEVENTF_LEFTUP) else CSGO.clientDLL[ClientOffsets.dwForceAttack] = 4.toByte() //Mouse release
+		}
+
 		if (forceSet) {
 			lastPunch.set(0F, 0F)
 		}
 	}
+	//}
 	
 	bone.set(when {
 		shotsFired >= SHIFT_TO_BODY_SHOTS -> BODY_BONE
