38b9ab7dfe85fa6b8d575bfd831ebc2c04bfcf60
AStupidRat <thedankestofmemerinos@gmail.com>
2019-04-29 08:42:53 -0500

    Hotfix -- updated version #, readme includes update information

--- src/main/kotlin/rat/poison/RatPoison.kt
+++ src/main/kotlin/rat/poison/RatPoison.kt
@@ -156,8 +156,6 @@ object App : ApplicationAdapter() {
     override fun render() {
         sync(OPENGL_FPS)
 
-        println(saving)
-
         if (!Thread.interrupted()) {
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
 
