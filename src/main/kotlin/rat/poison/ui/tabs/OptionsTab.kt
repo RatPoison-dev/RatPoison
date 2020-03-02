6359411da1a261d1b434942073432492f60b1493
TheFuckingRat <thedankestofmemerinos@gmail.com>
2020-03-02 10:14:10 -0600

    1.6 Initial Release

--- src/main/kotlin/rat/poison/ui/tabs/OptionsTab.kt
+++ src/main/kotlin/rat/poison/ui/tabs/OptionsTab.kt
@@ -36,7 +36,7 @@ class OptionsTab : Tab(false, false) {
     val menuKeyField = VisValidatableTextField(Validators.FLOATS)
     val stayFocused = VisCheckBoxCustom("Stay Focused", "MENU_STAY_FOCUSED")
     val debug = VisCheckBoxCustom("Debug", "DEBUG")
-    val discordLink = LinkLabel("Join Discord", "https://discord.gg/XWUjZs8")
+    val discordLink = LinkLabel("Join Discord", "https://discord.gg/J2uHTJ2")
 
     init {
         //Create UIAlpha Slider
--- src/main/kotlin/rat/poison/ui/uiHelpers/tables/AimTable.kt
+++ src/main/kotlin/rat/poison/ui/uiHelpers/tables/AimTable.kt
@@ -80,7 +80,7 @@ class AimTable: VisTable(true) {
                     "\n\nUpdate 1.6 removes aim strictness from aim settings" +
                     "\nOlder configs shouldn't break, but your aim settings" +
                     "\nmight need to be updated." +
-                    "\n\n Official discord server: https://discord.gg/XWUjZs8")
+                    "\n\n Official discord server: https://discord.gg/J2uHTJ2")
             dialog.setPosition(CSGO.gameWidth / 4F - dialog.width / 2F, CSGO.gameHeight.toFloat() / 2F)
             App.menuStage.addActor(dialog)
         }
