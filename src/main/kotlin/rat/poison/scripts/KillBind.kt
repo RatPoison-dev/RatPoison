1839eeaac5c2144afc358a3db7d48058f273837b
retart1337 <misha.domanov@mail.ru>
2020-08-01 17:52:42 +0300

    Refactor Kill Bind, disable post processing from config

--- src/main/kotlin/rat/poison/scripts/KillBind.kt
+++ src/main/kotlin/rat/poison/scripts/KillBind.kt
@@ -2,8 +2,7 @@ package rat.poison.scripts
 
 import rat.poison.curSettings
 import rat.poison.game.CSGO
-import rat.poison.game.entity.Entity
-import rat.poison.game.entity.EntityType
+import rat.poison.game.entity.*
 import rat.poison.game.entity.dead
 import rat.poison.game.entity.health
 import rat.poison.game.forEntities
@@ -14,60 +13,15 @@ import rat.poison.utils.every
 import rat.poison.utils.generalUtil.strToBool
 import rat.poison.utils.notInGame
 
-val killBinds = Array(256) { Binds() }
-data class Binds(var ent: Entity = 0L, var open: Boolean = true)
-private var totalHits = 0
+private var totalKills = me.kills()
 
 fun killBind() = every(600) {
-    if (!curSettings["KILL_BIND"].strToBool() || me.dead() || me <= 0 || notInGame) return@every
+    if (!curSettings["KILL_BIND"].strToBool() || me <= 0 || notInGame) return@every
 
-    forEntities(EntityType.CCSPlayer) {
-        val ent = it.entity
-
-        if (ent != me && ent > 0) {
-            val idx = emptySlot(it.entity)
-            if (idx != -1) {
-                killBinds[idx].apply {
-                    this.ent = ent
-                    open = false
-                }
-            }
-        }
+    val curKills = me.kills()
+    if (curKills != totalKills) {
+        totalKills = curKills
+        robot.keyPress(curSettings["KILL_BIND_KEY"].toInt())
+        robot.keyRelease(curSettings["KILL_BIND_KEY"].toInt())
     }
-
-    val curHits = CSGO.csgoEXE.int(me + NetVarOffsets.m_totalHitsOnServer)
-
-    for (i in killBinds.indices) {
-        if (!killBinds[i].open) {
-            if (killBinds[i].ent.health() <= 0) {
-                if (curHits == 0) {
-                    totalHits = 0
-                }
-                else if (totalHits != curHits) {
-                    // entity was likely killed by localPlayer
-                    totalHits = curHits
-                    killBinds[i].apply {
-                        open = true
-                        ent = 0L
-                    }
-                    robot.keyPress(curSettings["KILL_BIND_KEY"].toInt())
-                    robot.keyRelease(curSettings["KILL_BIND_KEY"].toInt())
-                }
-            }
-        }
-    }
-}
-
-
-private fun emptySlot(ent: Entity): Int {
-    var idx = -1
-
-    for (i in killBinds.indices) {
-        if (killBinds[i].open || killBinds[i].ent == ent) {
-            idx = i
-            break
-        }
-    }
-
-    return idx
 }
