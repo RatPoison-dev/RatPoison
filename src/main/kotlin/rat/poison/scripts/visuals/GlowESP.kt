d7f776b9cee4425e0fb714361de253913fb876e2
retart1337 <misha.domanov@mail.ru>
2020-09-27 13:08:49 +0300

    Revert "Refactor glow esp"
    This reverts commit b985457d

--- src/main/kotlin/rat/poison/scripts/visuals/GlowESP.kt
+++ src/main/kotlin/rat/poison/scripts/visuals/GlowESP.kt
@@ -44,30 +44,11 @@ internal fun glowEspEvery() = every(100, true) {
 	val showBombCarrier = curSettings["GLOW_SHOW_BOMB_CARRIER"].strToBool()
 	val showWeapons = curSettings["GLOW_SHOW_WEAPONS"].strToBool()
 	val showGrenades = curSettings["GLOW_SHOW_GRENADES"].strToBool()
-	val showDefuseKits = curSettings["GLOW_SHOW_DEFUSE_KITS"].strToBool()
 
 	val meTeam = me.team()
 	forEntities {
-		val isPlayer = it.type == EntityType.CCSPlayer
-		val isWeapon = it.type.weapon
-		val isDefuseKit = it.type == EntityType.CEconEntity
-		val isGrenade = it.type.grenade
-		val isBomb = (it.type == EntityType.CPlantedC4) || (it.type == EntityType.CC4)
-		val ent = it.entity
-		if (!isPlayer && !isWeapon && !isDefuseKit) return@forEntities
-
-
-		if (curSettings["BOX_SMOKE_CHECK"].strToBool()) {
-			if (lineThroughSmoke(ent)) {
-				return@forEntities
-			}
-		}
-
-		val onTeam = ent.team() == meTeam
-		if (isPlayer && !DANGER_ZONE && (ent == me || ent.dormant() || ent.dead() || (!showEnemies && !onTeam) || (!showTeam && onTeam))) return@forEntities
-		if (isWeapon && !showWeapons) return@forEntities
-		if (isDefuseKit && !showDefuseKits) return@forEntities
-		if (isGrenade && !showGrenades) return@forEntities
+		val entity = it.entity
+		if (entity <= 0 || me == entity || entity.dormant()) return@forEntities
 
 		val glowAddress = it.glowAddress
 		if (glowAddress <= 0) return@forEntities
@@ -76,64 +57,72 @@ internal fun glowEspEvery() = every(100, true) {
 		var color = "NIL"
 		var glowType = -1
 
-		if (isPlayer) {
-			if (showTarget && ent == espTARGET && espTARGET != -1L) {
-				glowType = curSettings["GLOW_TARGET_TYPE"].toGlowNum()
-				color = "GLOW_TARGET_COLOR"
-			} else if (showEnemies && !onTeam) {
-				glowType = curSettings["GLOW_ENEMY_TYPE"].toGlowNum()
-				color = when (bEnt > 0 && bEnt == ent && showBombCarrier) {
-					true -> {
-						glowType = curSettings["GLOW_BOMB_CARRIER_TYPE"].toGlowNum(); "GLOW_BOMB_CARRIER_COLOR"
-					}
-					else -> when (glowHealth) {
-						true -> "GLOW_HEALTH"
-						else -> "GLOW_ENEMY_COLOR"
-					}
-				}
-			} else if (showTeam && onTeam) {
-				glowType = curSettings["GLOW_TEAMMATE_TYPE"].toGlowNum()
-				color = when (bEnt > 0 && bEnt == ent && showBombCarrier) {
-					true -> {
-						glowType = curSettings["GLOW_BOMB_CARRIER_TYPE"].toGlowNum(); "GLOW_BOMB_CARRIER_COLOR"
-					}
-					else -> when (glowHealth) {
-						true -> "GLOW_HEALTH"
-						else -> "GLOW_TEAM_COLOR"
-					}
-				}
-			}
-		}
-		//if (isDefuseKit && showDefuseKits) {
-		//	glowType = curSettings["GLOW_DEFUSE_KITS_TYPE"].toGlowNum()
-		//	color = "GLOW_DEFUSE_KITS_COLOR"
-		//}
-		if (isBomb && showBomb) {
-			glowType = curSettings["GLOW_BOMB_TYPE"].toGlowNum()
-			color = when (curSettings["GLOW_BOMB_ADAPTIVE"].strToBool()) {
-				true -> if ((bombState.timeLeftToExplode > 10) || (bombState.gettingDefused && bombState.canDefuse) && bombState.planted) {
-					"GLOW_BOMB_ADAPTIVE_CAN_DEFUSE"
-				} else if ((bombState.timeLeftToExplode < 5) || (bombState.gettingDefused && !bombState.canDefuse) && bombState.planted) {
-					"GLOW_BOMB_ADAPTIVE_CANT_DEFUSE"
-				} else if (bombState.planted && bombState.timeLeftToExplode < 10 && bombState.timeLeftToExplode > 5) {
-					"GLOW_BOMB_ADAPTIVE_LITTLE_TIME"
-				} else {
-					"GLOW_BOMB_COLOR"
-				}
-				false -> "GLOW_BOMB_COLOR"
-			}
-		}
-		if (isWeapon && showWeapons) {
-			glowType = curSettings["GLOW_WEAPON_TYPE"].toGlowNum()
-			color = "GLOW_WEAPON_COLOR"
-		}
-		if (showGrenades && isGrenade) {
-			glowType = curSettings["GLOW_GRENADE_TYPE"].toGlowNum()
-			color = "GLOW_GRENADE_COLOR"
-		}
+		when (it.type) {
+			EntityType.CCSPlayer -> {
+				if (entity.dead()) return@forEntities
 
+				val entityTeam = entity.team()
+				val team = !DANGER_ZONE && meTeam == entityTeam
+				health = entity.health()
+
+				if (showTarget && entity == espTARGET && espTARGET != -1L) {
+					glowType = curSettings["GLOW_TARGET_TYPE"].toGlowNum()
+					color = "GLOW_TARGET_COLOR"
+				} else if (showEnemies && !team) {
+					glowType = curSettings["GLOW_ENEMY_TYPE"].toGlowNum()
+					color = when (bEnt > 0 && bEnt == entity && showBombCarrier) {
+						true -> { glowType = curSettings["GLOW_BOMB_CARRIER_TYPE"].toGlowNum(); "GLOW_BOMB_CARRIER_COLOR" }
+						else -> when (glowHealth) {
+							true -> "GLOW_HEALTH"
+							else -> "GLOW_ENEMY_COLOR"
+						}
+					}
+				} else if (showTeam && team) {
+					glowType = curSettings["GLOW_TEAMMATE_TYPE"].toGlowNum()
+					color = when (bEnt > 0 && bEnt == entity && showBombCarrier) {
+						true -> { glowType = curSettings["GLOW_BOMB_CARRIER_TYPE"].toGlowNum(); "GLOW_BOMB_CARRIER_COLOR" }
+						else -> when (glowHealth) {
+							true -> "GLOW_HEALTH"
+							else -> "GLOW_TEAM_COLOR"
+						}
+					}
+				}
+			}
+
+			EntityType.CPlantedC4, EntityType.CC4 -> if (showBomb) {
+				glowType = curSettings["GLOW_BOMB_TYPE"].toGlowNum()
+				color = when (curSettings["GLOW_BOMB_ADAPTIVE"].strToBool()) {
+					true -> if ((bombState.timeLeftToExplode > 10) || (bombState.gettingDefused && bombState.canDefuse) && bombState.planted) {
+						"GLOW_BOMB_ADAPTIVE_CAN_DEFUSE"
+					} else if ((bombState.timeLeftToExplode < 5) || (bombState.gettingDefused && !bombState.canDefuse) && bombState.planted) {
+						"GLOW_BOMB_ADAPTIVE_CANT_DEFUSE"
+					} else if (bombState.planted && bombState.timeLeftToExplode < 10 && bombState.timeLeftToExplode > 5) {
+						"GLOW_BOMB_ADAPTIVE_LITTLE_TIME"
+					} else {
+						"GLOW_BOMB_COLOR"
+					}
+					false -> "GLOW_BOMB_COLOR"
+				}
+			}
+
+			else ->
+				if (showWeapons && it.type.weapon) {
+					glowType = curSettings["GLOW_WEAPON_TYPE"].toGlowNum()
+					color = "GLOW_WEAPON_COLOR"
+				} else if (showGrenades && it.type.grenade) {
+					glowType = curSettings["GLOW_GRENADE_TYPE"].toGlowNum()
+					color = "GLOW_GRENADE_COLOR"
+				}
+		}
 
 		if (color != "NIL") {
+			if (curSettings["GLOW_SMOKE_CHECK"].strToBool()) {
+				if (lineThroughSmoke(entity)) {
+					glowAddress.glow(curSettings[color].strToColor(), -1)
+					return@forEntities
+				}
+			}
+
 			if (color == "GLOW_HEALTH") {
 				glowAddress.glow(Color((255 - 2.55 * health).toInt(), (2.55 * health).toInt(), 0, 1.0), glowType)
 			} else {
