2819e709d01fe99f88279acc2fe191c4767c181e
retart1337 <misha.domanov@mail.ru>
2020-10-05 17:03:29 +0300

    Migration cfg integration, small fixes in installer

---
+++ src/main/kotlin/rat/poison/utils/CFGMigration.kt
@@ -0,0 +1,18 @@
+package rat.poison.utils
+
+import rat.poison.SETTINGS_DIRECTORY
+import rat.poison.ui.tabs.loadCFG
+import java.io.File
+
+fun loadMigration() {
+    val file = File("$SETTINGS_DIRECTORY/CFGS/default_migration.cfg")
+    if (file.exists()) {
+        try {
+            loadCFG("default_migration", true)
+            println("Default migration config found and successfully loaded.")
+        }
+        catch (e: Exception) {
+            println("Default migration config found and wasn't successfully loaded.")
+        }
+    }
+}
