a35066b01a7d2c228527f9f364fa411b3f35c4a3
Jire <thomasgnappo@gmail.com>
2020-12-22 19:31:36 -0500

    Fixed the huge glitches with inline Vector

--- src/main/kotlin/rat/poison/utils/Vector.kt
+++ src/main/kotlin/rat/poison/utils/Vector.kt
@@ -1,17 +1,17 @@
 package rat.poison.utils
 
-import java.util.concurrent.ThreadLocalRandom
 import kotlin.math.abs
+import kotlin.math.roundToLong
 
 inline class Vector(val value: Long) {
 	constructor(x: Float = 0F, y: Float = 0F, z: Float = 0F) : this(vectorLong(x, y, z))
 	
 	val x: Float
-		get() = toFloat(value ushr 32)
+		get() = ((value ushr 42).toFloat() / FAST_VECTOR_PRECISION_DIV) - FAST_VECTOR_OFFSET
 	val y: Float
-		get() = toFloat((value ushr 16) and 0xFFFF)
+		get() = (((value ushr 21) and FAST_VECTOR_BITMASK).toFloat() / FAST_VECTOR_PRECISION_DIV) - FAST_VECTOR_OFFSET
 	val z: Float
-		get() = toFloat(value and 0xFFFF)
+		get() = ((value and FAST_VECTOR_BITMASK).toFloat() / FAST_VECTOR_PRECISION_DIV) - FAST_VECTOR_OFFSET
 	
 	fun set(x: Float, y: Float, z: Float = 0F) = Vector(x, y, z)
 	fun x(x: Float) = Vector(x, y, z)
@@ -44,7 +44,7 @@ inline class Vector(val value: Long) {
 	
 	fun distanceTo(target: Vector) = abs(x - target.x) + abs(y - target.y) + abs(z - target.z)
 	
-	fun dot(x: Float, y: Float, z: Float): Float = this.x * x + this.y * y + this.z * z
+	fun dot(x: Float, y: Float, z: Float) = dot(x, y, z, this.x, this.y, this.z)
 	
 	fun dst2(x: Float, y: Float, z: Float): Float {
 		val a = x - this.x
@@ -54,127 +54,26 @@ inline class Vector(val value: Long) {
 	}
 }
 
+fun dot(x: Float, y: Float, z: Float, tx: Float, ty: Float, tz: Float): Float = tx * x + ty * y + tz * z
+
+const val FAST_VECTOR_OFFSET = 9999F
+const val FAST_VECTOR_PRECISION_MULT = 100.0
+const val FAST_VECTOR_PRECISION_DIV = FAST_VECTOR_PRECISION_MULT.toFloat()
+const val FAST_VECTOR_BITMASK = 0x1FFFFFL
+
+fun vectorLong(x: Float = 0F, y: Float = 0F, z: Float = 0F): Long {
+	val x2 = ((x + FAST_VECTOR_OFFSET) * FAST_VECTOR_PRECISION_MULT).roundToLong() and FAST_VECTOR_BITMASK
+	val y2 = ((y + FAST_VECTOR_OFFSET) * FAST_VECTOR_PRECISION_MULT).roundToLong() and FAST_VECTOR_BITMASK
+	val z2 = ((z + FAST_VECTOR_OFFSET) * FAST_VECTOR_PRECISION_MULT).roundToLong() and FAST_VECTOR_BITMASK
+	
+	var vector = x2 shl 42
+	vector = vector or (y2 shl 21)
+	vector = vector or z2
+	return vector
+}
+
 typealias Angle = Vector
 
 fun angle(x: Float = 0F, y: Float = 0F): Angle = Vector(x, y)
 
 fun vector(x: Float = 0F, y: Float = 0F, z: Float = 0F): Vector = Vector(vectorLong(x, y, z))
-
-fun vectorLong(x: Float = 0F, y: Float = 0F, z: Float = 0F): Long {
-	val x2 = toHalf(x)
-	val y2 = toHalf(y)
-	val z2 = toHalf(z)
-	
-	var vector = x2 shl 32
-	vector = vector or (y2 shl 16)
-	vector = vector or z2
-	return vector
-}
-
-fun toHalf(f: Float): Long {
-	val bits = java.lang.Float.floatToRawIntBits(f)
-	val s = bits ushr FP32_SIGN_SHIFT
-	var e = bits ushr FP32_EXPONENT_SHIFT and FP32_SHIFTED_EXPONENT_MASK
-	var m = bits and FP32_SIGNIFICAND_MASK
-	var outE = 0
-	var outM = 0
-	if (e == 0xff) { // Infinite or NaN
-		outE = 0x1f
-		outM = if (m != 0) 0x200 else 0
-	} else {
-		e = e - FP32_EXPONENT_BIAS + EXPONENT_BIAS
-		if (e >= 0x1f) { // Overflow
-			outE = 0x1f
-		} else if (e <= 0) { // Underflow
-			if (e < -10) {
-				// The absolute fp32 value is less than MIN_VALUE, flush to +/-0
-			} else {
-				// The fp32 value is a normalized float less than MIN_NORMAL,
-				// we convert to a denorm fp16
-				m = m or 0x800000
-				val shift = 14 - e
-				outM = m shr shift
-				val lowm = m and (1 shl shift) - 1
-				val hway = 1 shl shift - 1
-				// if above halfway or exactly halfway and outM is odd
-				if (lowm + (outM and 1) > hway) {
-					// Round to nearest even
-					// Can overflow into exponent bit, which surprisingly is OK.
-					// This increment relies on the +outM in the return statement below
-					outM++
-				}
-			}
-		} else {
-			outE = e
-			outM = m shr 13
-			// if above halfway or exactly halfway and outM is odd
-			if ((m and 0x1fff) + (outM and 0x1) > 0x1000) {
-				// Round to nearest even
-				// Can overflow into exponent bit, which surprisingly is OK.
-				// This increment relies on the +outM in the return statement below
-				outM++
-			}
-		}
-	}
-	// The outM is added here as the +1 increments for outM above can
-	// cause an overflow in the exponent bit which is OK.
-	return (s shl SIGN_SHIFT or (outE shl EXPONENT_SHIFT) + outM).toShort().toLong()
-}
-
-fun toFloat(h: Long): Float {
-	val bits: Int = h.toInt() and 0xffff
-	val s = bits and SIGN_MASK
-	val e = bits ushr EXPONENT_SHIFT and SHIFTED_EXPONENT_MASK
-	val m = bits and SIGNIFICAND_MASK
-	var outE = 0
-	var outM = 0
-	if (e == 0) { // Denormal or 0
-		if (m != 0) {
-			// Convert denorm fp16 into normalized fp32
-			var o = java.lang.Float.intBitsToFloat(FP32_DENORMAL_MAGIC + m)
-			o -= FP32_DENORMAL_FLOAT
-			return if (s == 0) o else -o
-		}
-	} else {
-		outM = m shl 13
-		if (e == 0x1f) { // Infinite or NaN
-			outE = 0xff
-			if (outM != 0) { // SNaNs are quieted
-				outM = outM or FP32_QNAN_MASK
-			}
-		} else {
-			outE = e - EXPONENT_BIAS + FP32_EXPONENT_BIAS
-		}
-	}
-	val out = s shl 16 or (outE shl FP32_EXPONENT_SHIFT) or outM
-	return java.lang.Float.intBitsToFloat(out)
-}
-
-const val SIGN_SHIFT = 15
-const val SIGN_MASK = 0x8000
-const val SHIFTED_EXPONENT_MASK = 0x1f
-const val SIGNIFICAND_MASK = 0x3ff
-const val EXPONENT_SHIFT = 10
-const val EXPONENT_BIAS = 15
-private const val FP32_SIGN_SHIFT = 31
-private const val FP32_EXPONENT_SHIFT = 23
-private const val FP32_SHIFTED_EXPONENT_MASK = 0xff
-private const val FP32_SIGNIFICAND_MASK = 0x7fffff
-private const val FP32_EXPONENT_BIAS = 127
-private const val FP32_QNAN_MASK = 0x400000
-private const val FP32_DENORMAL_MAGIC = 126 shl 23
-private val FP32_DENORMAL_FLOAT = java.lang.Float.intBitsToFloat(FP32_DENORMAL_MAGIC)
-
-fun main() {
-	val tlr = ThreadLocalRandom.current()
-	for (i in 0..1000000) {
-		val x = tlr.nextFloat()
-		val y = tlr.nextFloat()
-		val z = tlr.nextFloat()
-		
-		val v = vector(x, y, z)
-		if (abs(v.x - x) > 0.001 || abs(v.y - y) > 0.001 || abs(v.z - z) > 0.001) {
-			println("FAILED x $x should be ${v.x}, y $y should be ${v.y}, z $z should be ${v.z}")
-		}
-	}
-}
