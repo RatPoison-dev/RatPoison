6b456df4b3c1a8c263220d7c703a115a28c5f772
Jire <thomasgnappo@gmail.com>
2021-01-05 17:09:08 -0500

    fixed killbind invalid key checking

--- src/main/kotlin/rat/poison/scripts/KillBind.kt
+++ src/main/kotlin/rat/poison/scripts/KillBind.kt
@@ -10,12 +10,14 @@ import rat.poison.utils.every
 private var totalKills = me.kills()
 
 fun killBind() = every(600, inGameCheck = true) {
-    if (!curSettings.bool["KILL_BIND"] || me <= 0 || meDead) return@every
-
-    val curKills = me.kills()
-    if (curKills != totalKills) {
-        totalKills = curKills
-        robot.keyPress(curSettings.int["KILL_BIND_KEY"])
-        robot.keyRelease(curSettings.int["KILL_BIND_KEY"])
-    }
+	if (!curSettings.bool["KILL_BIND"] || me <= 0 || meDead) return@every
+	val key = curSettings.int["KILL_BIND_KEY"]
+	if (key < 0) return@every
+	
+	val curKills = me.kills()
+	if (curKills != totalKills) {
+		totalKills = curKills
+		robot.keyPress(key)
+		robot.keyRelease(key)
+	}
 }
