952fef906b5c84d9d49959e4382fc49323efbd58
SPRAVEDLIVO <misha.domanov@mail.ru>
2021-04-10 10:55:40 +0300

    tomfoolery

--- src/main/kotlin/rat/poison/scripts/Backtrack.kt
+++ src/main/kotlin/rat/poison/scripts/Backtrack.kt
@@ -62,10 +62,10 @@ private const val inputMemorySize = 253
 private val inputMemory = threadLocalPointer(inputMemorySize)
 private const val oldUserCmdMemorySize = 100
 private const val newUserCmdMemorySize = 100
-//private val oldUserCmdMemory = threadLocalPointer(oldUserCmdMemorySize)
-//private val newUserCMDMemory = threadLocalPointer(newUserCmdMemorySize)
-//private val oldUserCMD = UserCMD()
-//private val newUserCMD = UserCMD()
+private val oldUserCmdMemory = threadLocalPointer(oldUserCmdMemorySize)
+private val newUserCMDMemory = threadLocalPointer(newUserCmdMemorySize)
+private val oldUserCMD = UserCMD()
+private val newUserCMD = UserCMD()
 fun attemptBacktrack(): Boolean {
     if (((curSettings.bool["BACKTRACK_SPOTTED"] && bestBacktrackTarget.spotted()) || !curSettings.bool["BACKTRACK_SPOTTED"]) && bestBacktrackTarget > 0L && haveGvars) {
         inBacktrack = true
@@ -88,8 +88,12 @@ fun attemptBacktrack(): Boolean {
         }
 
         //Check invalid?
-        val oldUserCMD = memToUserCMD(csgoEXE.read(oldUserCMDptr, 100)!!) //TODO what
-        var userCMD = memToUserCMD(csgoEXE.read(userCMDptr, 100)!!)
+        val oldUserCmdMemory = oldUserCmdMemory.get()
+        csgoEXE.read(oldUserCMDptr, oldUserCmdMemory, oldUserCmdMemorySize)
+        val oldUserCMD = memToUserCMD(oldUserCmdMemory, oldUserCMD)
+        val newUserCmdMemory = newUserCMDMemory.get()
+        csgoEXE.read(userCMDptr, newUserCmdMemory, newUserCmdMemorySize)
+        var userCMD = memToUserCMD(newUserCmdMemory, newUserCMD)
 
         userCMD = fixUserCMD(userCMD, oldUserCMD)
 
