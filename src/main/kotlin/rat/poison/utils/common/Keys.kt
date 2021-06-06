8c589794d450b9d8cf51b6fdeac8e3ae62cb4eb9
SPRAVEDLIVO <misha.domanov@mail.ru>
2021-06-06 14:36:27 +0300

    print menu key

--- src/main/kotlin/rat/poison/utils/common/Keys.kt
+++ src/main/kotlin/rat/poison/utils/common/Keys.kt
@@ -1,7 +1,6 @@
 package rat.poison.utils.common
 
 import org.jire.arrowhead.keyPressed
-import rat.poison.utils.duplicationsMap
 
 fun keyPressed(virtualKeyCode: Int): Boolean {
     val keyMap = duplicationsMap[virtualKeyCode]
